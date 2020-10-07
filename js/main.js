5b2c0c70ebfbe4ceb2ef3fe7e6ef7947d6e57260
AKN2807 <akn740728@gmail.com>
2020-10-07 22:59:46 +0300

    Убрана лишняя переменная

--- js/main.js
+++ js/main.js
@@ -75,11 +75,10 @@ let createListAd = function (counter) {
 let randomListAD = createListAd(AD_COUNTER); // Создаём переменную в которую сохраням массив сгенерированных случайных объявлений.
 
 // Функция создания метки на карте из случайного объявления.
-let createMapPin = function (listAD) { // Создаём метку на карте по шаблону.
+let createMapPin = function (randomAd) { // Создаём метку на карте по шаблону.
   let simelarMapPinTemplate = document.querySelector(`#pin`)
     .content
     .querySelector(`.map__pin`); // Находим button в шаблоне, которой будем менять свойства.
-  let randomAd = listAD;
   let randomMapPin = simelarMapPinTemplate.cloneNode(true); // Клонируем шаблон
   let locationPinX = randomAd.location.x - MARKER_SIZE_X / 2 + `px`; // Получаем координаты по X объекта с учётом размеров изображения указателя
   let locationPinY = randomAd.location.y - MARKER_SIZE_Y + `px`; // Получаем координаты по Y объекта с учётом размеров изображения указателя
